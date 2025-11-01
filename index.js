d900f1f861608b337d6cff50678ab60f6bd79dcd
anand <anand.k4756@gmail.com>
2025-11-01 12:43:25 +0530

    allowed urls

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ require("./config/db");
 const headerRoutes = require("./routes/header.route");
 
 const app = express();
-const allowedOrigins = ["http://localhost:3000"];
+const allowedOrigins = ["http://localhost:3000", process.env.CLIENT_URL];
 
 app.use(
   cors({
